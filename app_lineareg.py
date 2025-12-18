2dd1a7e552764f071a309f6d8e11b0274b3c88d4
vedapriya-1212 <vedapriya1212gudipati@gmail.com>
2025-12-18 10:08:12 +0530

    app

---
+++ app_lineareg.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
+#page configuration
+st.set_page_config(page_title="Linear Regression", layout="centered")
