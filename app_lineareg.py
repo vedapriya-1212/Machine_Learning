2e07dc0739cc4fbd5ece22dcced41c1753a1de27
vedapriya-1212 <vedapriya1212gudipati@gmail.com>
2025-12-18 12:27:27 +0530

    requirements

--- app_lineareg.py
+++ app_lineareg.py
@@ -11,3 +11,100 @@ from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 #page configuration
 st.set_page_config(page_title="Linear Regression", layout="centered")
+
+#load css
+def load_css(file):
+    with open(file) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+load_css("styles.css")
+
+#title
+
+st.markdown("""
+            <div class="card">
+            <h1>Linear Regression </h1>
+            <p>Predict <b> Tip Amount</b> from <b> Total Bill </b> using Linear Regression...</p>
+            </div>
+            """, unsafe_allow_html=True)
+
+#load data
+
+@st.cache_data
+def load_data():
+    return sns.load_dataset("tips")
+df = load_data()
+
+#dataset preview
+st.markdown('<div class="card">', unsafe_allow_html=True)
+st.subheader("Dataset Preview")
+st.dataframe(df.head())
+st.markdown('</div>', unsafe_allow_html=True)
+
+#prepare data
+
+x, y = df[["total_bill"]], df["tip"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+#train model
+model = LinearRegression()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+
+#metrics
+mae= mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+adj_r2=1-(1-r2)*(len(y)-1)/(len(y)-x.shape[1]-1)
+
+#visualization
+st.markdown('<div class="card">', unsafe_allow_html=True)
+st.subheader("Total Bill vs Tip")
+fig, ax= plt.subplots()
+ax.scatter(df["total_bill"], df["tip"], alpha=0.6, color='black')
+ax.plot(df["total_bill"], model.predict(scaler.transform(x)), color='black', linewidth=2)
+ax.set_xlabel("Total Bill ($)")
+ax.set_ylabel("Tip ($)")
+st.pyplot(fig)
+
+st.markdown('</div>', unsafe_allow_html=True)
+
+#performance metrics
+
+st.markdown('<div class="card">', unsafe_allow_html=True)
+st.subheader("Model Performance Metrics")
+
+c1,c2,= st.columns(2)
+c1.metric("Mean Absolute Error (MAE)", f"{mae:.2f}")
+c2.metric("Root Mean Squared Error (RMSE)", f"{rmse:.2f}")
+c3,c4= st.columns(2)
+c3.metric("R-squared (R²)", f"{r2:.3f}")
+c4.metric("Adjusted R-squared", f"{adj_r2:.3f}")
+st.markdown('</div>', unsafe_allow_html=True)
+
+#m & c
+st.markdown(f"""
+<div class="card">
+<h3>Model Interception</h3>
+            <p><b>co-efficient:</b> {model.coef_[0]:.3f}<br>
+            <b>intercept:</b> {model.intercept_:.3f}</p>
+</div>""", unsafe_allow_html=True)
+
+#prediction
+st.markdown('<div class="card">', unsafe_allow_html=True)
+
+bill_amount = st.slider(
+    "Enter the total bill amount:",
+    float(df["total_bill"].min()),
+    float(df["total_bill"].max()),
+    30.0
+)
+bill_amount_scaled = scaler.transform(
+    pd.DataFrame([[bill_amount]], columns=["total_bill"])
+)
+tip = model.predict(bill_amount_scaled)[0]
+st.markdown(f'<div class="prediction-box">Predicted Tip Amount: ${tip:.2f} </div>', unsafe_allow_html=True)
+st.markdown('</div>', unsafe_allow_html=True)           
